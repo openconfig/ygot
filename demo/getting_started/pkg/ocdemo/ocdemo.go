32a0f0bf0830d864d161143613ca6d3794f43872
Rob Shakir <robjs@google.com>
2017-08-16 10:26:42 -0700

    Add place holder ocdemo file to ensure no empty packages.
      * (A) demo/getting_started/pkg/ocdemo/ocdemo.go
        - Add an ocdemo.go file which declares the ocdemo package to
          ensure that the pkg/ocdemo package is not empty for Go
          usage.

---
+++ demo/getting_started/pkg/ocdemo/ocdemo.go
@@ -0,0 +1,6 @@
+package ocdemo
+
+// This file is a placeholder in order to ensure that Go does not
+// find this directory to contain an empty package. The structs
+// required for the getting started demo should be generated through
+// go generate within the getting_started directory.
