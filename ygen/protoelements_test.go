6b790ab223d5c4ef95974ed7ab4693fb28b7323e
Rob Shakir <robjs@google.com>
2017-08-18 21:01:57 -0700

    Add negative testing and refactor test list.
     * (M) ygen/protoelements_test.go
       - Negative testing of cases that are currently unmapped from
         goyang.
       - Refactor test such that there is a reduced amount of boilerplate.

--- ygen/protoelements_test.go
+++ ygen/protoelements_test.go
@@ -23,69 +23,66 @@ import (
 func TestYangTypeToProtoType(t *testing.T) {
 	tests := []struct {
 		name    string
-		in      resolveTypeArgs
+		in      []resolveTypeArgs
 		want    mappedType
 		wantErr bool
 	}{{
-		name: "int8",
-		in:   resolveTypeArgs{yangType: &yang.YangType{Kind: yang.Yint8}},
+		name: "integer types",
+		in: []resolveTypeArgs{
+			{yangType: &yang.YangType{Kind: yang.Yint8}},
+			{yangType: &yang.YangType{Kind: yang.Yint16}},
+			{yangType: &yang.YangType{Kind: yang.Yint32}},
+			{yangType: &yang.YangType{Kind: yang.Yint64}},
+		},
 		want: mappedType{nativeType: "ywrapper.IntValue"},
 	}, {
-		name: "int16",
-		in:   resolveTypeArgs{yangType: &yang.YangType{Kind: yang.Yint16}},
-		want: mappedType{nativeType: "ywrapper.IntValue"},
-	}, {
-		name: "int32",
-		in:   resolveTypeArgs{yangType: &yang.YangType{Kind: yang.Yint32}},
-		want: mappedType{nativeType: "ywrapper.IntValue"},
-	}, {
-		name: "int64",
-		in:   resolveTypeArgs{yangType: &yang.YangType{Kind: yang.Yint64}},
-		want: mappedType{nativeType: "ywrapper.IntValue"},
-	}, {
-		name: "uint8",
-		in:   resolveTypeArgs{yangType: &yang.YangType{Kind: yang.Yuint8}},
+		name: "unsigned integer types",
+		in: []resolveTypeArgs{
+			{yangType: &yang.YangType{Kind: yang.Yuint8}},
+			{yangType: &yang.YangType{Kind: yang.Yuint16}},
+			{yangType: &yang.YangType{Kind: yang.Yuint32}},
+			{yangType: &yang.YangType{Kind: yang.Yuint64}},
+		},
 		want: mappedType{nativeType: "ywrapper.UintValue"},
 	}, {
-		name: "uint16",
-		in:   resolveTypeArgs{yangType: &yang.YangType{Kind: yang.Yuint16}},
-		want: mappedType{nativeType: "ywrapper.UintValue"},
-	}, {
-		name: "uint32",
-		in:   resolveTypeArgs{yangType: &yang.YangType{Kind: yang.Yuint32}},
-		want: mappedType{nativeType: "ywrapper.UintValue"},
-	}, {
-		name: "uint64",
-		in:   resolveTypeArgs{yangType: &yang.YangType{Kind: yang.Yuint64}},
-		want: mappedType{nativeType: "ywrapper.UintValue"},
-	}, {
-		name: "bool",
-		in:   resolveTypeArgs{yangType: &yang.YangType{Kind: yang.Ybool}},
-		want: mappedType{nativeType: "ywrapper.BoolValue"},
-	}, {
-		name: "empty",
-		in:   resolveTypeArgs{yangType: &yang.YangType{Kind: yang.Yempty}},
+		name: "bool types",
+		in: []resolveTypeArgs{
+			{yangType: &yang.YangType{Kind: yang.Ybool}},
+			{yangType: &yang.YangType{Kind: yang.Yempty}},
+		},
 		want: mappedType{nativeType: "ywrapper.BoolValue"},
 	}, {
 		name: "string",
-		in:   resolveTypeArgs{yangType: &yang.YangType{Kind: yang.Ystring}},
+		in:   []resolveTypeArgs{{yangType: &yang.YangType{Kind: yang.Ystring}}},
 		want: mappedType{nativeType: "ywrapper.StringValue"},
 	}, {
 		name: "decimal64",
-		in:   resolveTypeArgs{yangType: &yang.YangType{Kind: yang.Ydecimal64}},
+		in:   []resolveTypeArgs{{yangType: &yang.YangType{Kind: yang.Ydecimal64}}},
 		want: mappedType{nativeType: "ywrapper.Decimal64Value"},
+	}, {
+		name: "unmapped types",
+		in: []resolveTypeArgs{
+			{yangType: &yang.YangType{Kind: yang.Yunion}},
+			{yangType: &yang.YangType{Kind: yang.Yenum}},
+			{yangType: &yang.YangType{Kind: yang.Yidentityref}},
+			{yangType: &yang.YangType{Kind: yang.Ybinary}},
+			{yangType: &yang.YangType{Kind: yang.Ybits}},
+		},
+		wantErr: true,
 	}}
 
 	for _, tt := range tests {
 		s := newGenState()
-		got, err := s.yangTypeToProtoType(tt.in)
-		if (err != nil) != tt.wantErr {
-			t.Errorf("%s: yangTypeToProtoType(%v): got unexpected error: %v", tt.name, tt.in, err)
-			continue
-		}
+		for _, st := range tt.in {
+			got, err := s.yangTypeToProtoType(st)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("%s: yangTypeToProtoType(%v): got unexpected error: %v", tt.name, tt.in, err)
+				continue
+			}
 
-		if diff := pretty.Compare(got, tt.want); diff != "" {
-			t.Errorf("%s: yangTypeToProtoType(%v): did not get correct type, diff(-got,+want):\n%s", tt.name, tt.in, diff)
+			if diff := pretty.Compare(got, tt.want); diff != "" {
+				t.Errorf("%s: yangTypeToProtoType(%v): did not get correct type, diff(-got,+want):\n%s", tt.name, tt.in, diff)
+			}
 		}
 	}
 }
