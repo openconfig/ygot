25405cb61073c2a121c396766c8482f72a64b5c7
Rob Shakir <robjs@google.com>
2017-10-03 20:51:03 +1100

    Add support for rendering and update design doc.

--- ygot/render.go
+++ ygot/render.go
@@ -33,7 +33,9 @@ const (
 	// BinaryTypeName is the name of the type that is used for YANG
 	// binary fields in the output structs.
 	BinaryTypeName string = "Binary"
-	EmptyTypeName  string = "YANGEmpty"
+	// EmptyTypeName is the name of the type that is used for YANG
+	// empty fields in the output structs.
+	EmptyTypeName string = "YANGEmpty"
 )
 
 // path stores the elements of a path for a particular leaf,
@@ -1119,6 +1121,17 @@ func constructJSONValue(field reflect.Value, parentMod string, args jsonOutputCo
 		}
 		if args.jType == RFC7951 {
 			value = writeIETFScalarJSON(value)
+		}
+	case reflect.Bool:
+		// A non-pointer field of type boolean is an empty leaf within the YANG schema.
+		// For RFC7951 this is represented as a null JSON array (i.e., [null]), if the
+		// case that this leaf is present in internal JSON, then it is rendered as 'true'
+		// only if the leaf is set, otherwise it is nil.
+		switch {
+		case args.jType == RFC7951 && field.Type().Name() == EmptyTypeName && field.Bool():
+			value = []interface{}{nil}
+		case field.Bool():
+			value = true
 		}
 	default:
 		return nil, fmt.Errorf("got unexpected field type, was: %v", field.Kind())
