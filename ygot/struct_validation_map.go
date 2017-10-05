67952b5130babacad36046489aa97e94f37074d4
Rob Shakir <robjs@google.com>
2017-10-05 18:01:59 +1100

    Make sure tests actually run - fix ygot tests.

--- ygot/struct_validation_map.go
+++ ygot/struct_validation_map.go
@@ -402,13 +402,13 @@ func copyStruct(dstVal, srcVal reflect.Value) error {
 // copyPtrField copies srcField to dstField. srcField and dstField must be
 // reflect.Value structs which represent pointers.
 func copyPtrField(dstField, srcField reflect.Value) error {
-	if srcField.IsNil() || !srcField.IsValid() {
-		return nil
-	}
-
 	if !util.IsValuePtr(srcField) {
 		return fmt.Errorf("received non-ptr type: %v", srcField.Kind())
 	}
+
+	if srcField.IsNil() || !srcField.IsValid() {
+		return nil
+  }
 
 	// Check for struct ptr, or ptr to avoid panic.
 	if util.IsValueStructPtr(srcField) {
