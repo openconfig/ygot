3e429b4df591171baed380389a723d6140054abe
Rob Shakir <robjs@google.com>
2017-11-01 07:50:31 -0700

    Update test dependencies.

--- demo/protobuf_getting_started/update.sh
+++ demo/protobuf_getting_started/update.sh
@@ -13,17 +13,15 @@ clean() {
 clean
 rm -rf ribproto
 
-git clone https://github.com/openconfig/public.git
-mkdir deps
-cp ../getting_started/yang/{ietf,iana}* deps
 go run ../../proto_generator/protogenerator.go \
   -generate_fakeroot \
    -base_import_path="github.com/openconfig/ygot/demo/protobuf_getting_started/ribproto" \
-  -path=public,deps -output_dir=ribproto \
+  -path=yang -output_dir=ribproto \
   -enum_package_name=enums -package_name=openconfig \
   -exclude_modules=ietf-interfaces \
-  public/release/models/rib/openconfig-rib-bgp.yang
+  yang/rib/openconfig-rib-bgp.yang
 
+go get -u github.com/google/protobuf
 proto_imports=".:${GOPATH}/src/github.com/google/protobuf/src:${GOPATH}/src"
 find ribproto -name "*.proto" | while read l; do
   protoc -I=$proto_imports --go_out=. $l
