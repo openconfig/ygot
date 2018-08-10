2c274f9aaa5b16c56689b7a78a3d7f16180ab138
Rob Shakir <robjs@google.com>
2018-08-09 20:53:34 -0700

    Fix bug with identity provenance identification.
     * (M) ygen/*
       - Fix a bug whereby if an identity was defined in a submodule
         its defining module would be identified as the submodule,
         rather than the parent module as is required by rfc6020.

--- ygen/yang_helpers.go
+++ ygen/yang_helpers.go
@@ -138,10 +138,9 @@ func removePrefix(s string) string {
 	return strings.Split(s, ":")[1]
 }
 
-// parentModuleName returns the name of the module that defined the yang.Node
-// supplied as the node argument. If the discovered root node of the node is found
-// to be a submodule, the name of the parent module is returned.
-func parentModuleName(node yang.Node) string {
+// definingModule returns the name of the module that defined the yang.Node
+// supplied. If node is within a submodule, the parent module name is returned.
+func definingModule(node yang.Node) yang.Node {
 	var definingMod yang.Node
 	definingMod = yang.RootNode(node)
 	if definingMod.Kind() == "submodule" {
@@ -149,7 +148,21 @@ func parentModuleName(node yang.Node) string {
 		mod := definingMod.(*yang.Module)
 		definingMod = mod.BelongsTo
 	}
+	return definingMod
+}
 
+// parentModuleName returns the name of the module or submodule that defined
+// the supplied node.
+func parentModuleName(node yang.Node) string {
+	return definingModule(node).NName()
+}
+
+// parentModulePrettyName returns the name of the module that defined the yang.Node
+// supplied as the node argument. If the discovered root node of the node is found
+// to be a submodule, the name of the parent module is returned. If the root has
+// a camel case extension, this is returned rather than the actual module name.
+func parentModulePrettyName(node yang.Node) string {
+	definingMod := definingModule(node)
 	if name, ok := camelCaseNameExt(definingMod.Exts()); ok {
 		return name
 	}
