b07d7ed4e33e00880150088d875f2dfb6cb12a75
Rob Shakir <robjs@google.com>
2018-09-27 10:06:57 -0700

    Update scripts/coverage.sh to exclude tests. (#238)
    * (M) scripts/coverage.sh
      - Avoid testing coverage for test packages that are themselves
        tests.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,7 +2,7 @@
 
 go get github.com/go-playground/overalls && go get github.com/mattn/goveralls
 
-overalls -project=github.com/openconfig/ygot -covermode=count -ignore=".git,vendor,demo,experimental/ygotutils,generator,ytypes/schema_tests"
+overalls -project=github.com/openconfig/ygot -covermode=count -ignore=".git,vendor,integration_tests,ygot/schema_tests,ygen/schema_tests,demo,experimental/ygotutils,generator,ytypes/schema_tests"
 goveralls -coverprofile=overalls.coverprofile -service travis-ci
 
 
