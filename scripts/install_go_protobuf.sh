628eab8e4c9fb6baa3a199b8080c677c8090324a
wenovus <wenbli@google.com>
2020-03-19 19:22:24 -0700

    Attempt to not use a protoc-gen-go install script

--- scripts/install_go_protobuf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-GIT_TAG=v1.20.0
-GP=$(go env GOPATH)
-go get -u google.golang.org/protobuf/cmd/protoc-gen-go 
-git -C $GOPATH/src/google.golang.org/protobuf checkout $GIT_TAG 
-go install google.golang.org/protobuf/cmd/protoc-gen-go
