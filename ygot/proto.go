40a57e464acae3a5ea401b2c8a85e879540a092f
Rob Shakir <robjs@google.com>
2020-03-19 19:49:52 -0700

    Add initial mapping of proto messages to gNMI paths. (#359)
    * Add initial mapping of proto messages to gNMI paths.
    
     * (M) proto/{yext,ywrapper}
      - Regenerate protos using new versions of protoc-gen-go to take
        advantage of new protobuf/protoreflect APIs.
     * (M) ygot/*
      - Add initial functions to map from input proto to <path, value>
        pairs. Currently handles basic types.
      - Add example proto for tests.
     * Moved to Go modules

---
+++ ygot/proto.go
@@ -0,0 +1,63 @@
+package ygot
+
+import (
+	"fmt"
+	"strings"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/descriptorpb"
+
+	yextpb "github.com/openconfig/ygot/proto/yext"
+	wpb "github.com/openconfig/ygot/proto/ywrapper"
+)
+
+// pathsFromProto returns, from a populated proto, a map between the YANG schema
+// path (as specified in the yext.schemapath extension) and the value populated in
+// the message.
+func pathsFromProto(p proto.Message) (map[string]interface{}, error) {
+	m := p.ProtoReflect()
+	pp := map[string]interface{}{}
+	var rangeErr error
+	m.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+		po := fd.Options().(*descriptorpb.FieldOptions)
+		if ex := proto.GetExtension(po, yextpb.E_Schemapath).(string); ex != "" {
+			// Set to scalar value by default -- we extract the value from the
+			// wrapper message, or child messages if required.
+			val := v.Interface()
+			if fd.Kind() == protoreflect.MessageKind {
+				switch t := v.Message().Interface().(type) {
+				case *wpb.BoolValue:
+					val = t.GetValue()
+				case *wpb.BytesValue:
+					val = t.GetValue()
+				case *wpb.Decimal64Value:
+					rangeErr = fmt.Errorf("unhandled type, decimal64")
+					return false
+				case *wpb.IntValue:
+					val = t.GetValue()
+				case *wpb.StringValue:
+					val = t.GetValue()
+				case *wpb.UintValue:
+					val = t.GetValue()
+				case proto.Message:
+					rangeErr = fmt.Errorf("unknown type as field value, type: %T, value: %v", ex, ex)
+					return false
+				}
+			}
+
+			// Handle cases where there is >1 path specified for a field based on
+			// path compression.
+			for _, path := range strings.Split(ex, "|") {
+				pp[path] = val
+			}
+		}
+		return true
+	})
+
+	if rangeErr != nil {
+		return nil, rangeErr
+	}
+
+	return pp, nil
+}
---
+++ ygot/testdata/exschemapath/gen.go
@@ -0,0 +1,5 @@
+// package exschemapath contains generated code for the exschemapath protobuf.
+package exschemapath
+
+// Generate command to create the protobuf .pb.go file.
+//go:generate sh -c "cd $GOPATH/src && protoc --proto_path=. --go_out=:. github.com/openconfig/ygot/ygot/testdata/exschemapath/exschemapath.proto"
