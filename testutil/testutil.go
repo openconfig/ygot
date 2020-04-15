61346c6f0cac40af05b7b5759650aadc70f44a42
wenovus <wenbli@google.com>
2020-04-15 12:05:41 -0700

    Revert cmpopts.IgnoreUnexported for testutil.NotificationSetEqual

--- testutil/testutil.go
+++ testutil/testutil.go
@@ -131,7 +131,7 @@ func SubscribeResponseSetEqual(a, b []*gnmipb.SubscribeResponse) bool {
 func NotificationSetEqual(a, b []*gnmipb.Notification, opts ...ComparerOpt) bool {
 	ignoreTS := hasIgnoreTimestamp(opts)
 	cmps := comparers(opts)
-	cmps = append(cmps, []cmp.Option{cmpopts.SortSlices(UpdateLess), cmpopts.IgnoreUnexported(gnmipb.Update{}), cmpopts.EquateEmpty()}...)
+	cmps = append(cmps, []cmp.Option{cmpopts.SortSlices(UpdateLess), cmpopts.EquateEmpty()}...)
 
 	for _, an := range a {
 		var matched bool
