ae0a39cb2e72dbf8c60ad0fbe5f2ce0a9908f4e6
Rob Shakir <robjs@google.com>
2021-06-10 10:04:30 -0700

    Add initial implementation of unmarshalling gNMI Notifications->pb. (#536)
    * Add initial implementation of unmarshalling gNMI Notifications->pb.
    
     * (M) proto/proto.go
     * (M) proto/proto_test.go
      - Add support for unmarshalling a map of gNMI.Path->value into
        a ygot-generated protobuf. This is done by ranging over the
        protobuf message and using the annotated paths.
      - Initial implementation just supporting string type.
    
    * Address review comments.
    
    * Address review comment.

--- protomap/proto_test.go
+++ protomap/proto_test.go
@@ -289,3 +289,63 @@ func TestPathsFromProtoInternal(t *testing.T) {
 		})
 	}
 }
+
+func TestProtoFromPaths(t *testing.T) {
+	tests := []struct {
+		desc             string
+		inProto          proto.Message
+		inVals           map[*gpb.Path]interface{}
+		inPrefix         *gpb.Path
+		wantProto        proto.Message
+		wantErrSubstring string
+	}{{
+		desc:             "nil proto",
+		wantErrSubstring: "nil protobuf",
+	}, {
+		desc:    "string field",
+		inProto: &epb.ExampleMessage{},
+		inVals: map[*gpb.Path]interface{}{
+			mustPath("/string"): "hello",
+		},
+		wantProto: &epb.ExampleMessage{
+			Str: &wpb.StringValue{Value: "hello"},
+		},
+	}, {
+		desc:    "wrong field type",
+		inProto: &epb.ExampleMessage{},
+		inVals: map[*gpb.Path]interface{}{
+			mustPath("/string"): 42,
+		},
+		wantErrSubstring: "got non-string value for string field",
+	}, {
+		desc:    "not a wrapper message",
+		inProto: &epb.ExampleMessage{},
+		inVals: map[*gpb.Path]interface{}{
+			mustPath("/message"): &gpb.Path{},
+		},
+		wantErrSubstring: "unimplemented",
+	}, {
+		desc:    "field that is not directly a child",
+		inProto: &epb.ExampleMessage{},
+		inVals: map[*gpb.Path]interface{}{
+			mustPath("/one/two/three"): "ignored",
+		},
+		wantProto: &epb.ExampleMessage{},
+	}}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			err := ProtoFromPaths(tt.inProto, tt.inVals, tt.inPrefix)
+			if err != nil {
+				if diff := errdiff.Substring(err, tt.wantErrSubstring); diff != "" {
+					t.Fatalf("did not get expected error, %s", diff)
+				}
+				return
+			}
+
+			if diff := cmp.Diff(tt.inProto, tt.wantProto, protocmp.Transform()); diff != "" {
+				t.Fatalf("did not get expected results, diff(-got,+want):\n%s", diff)
+			}
+		})
+	}
+}
