28175afe24f0d0a28e68d5ac9fa1bc35f7a8d10e
wenovus <wenbli@google.com>
2021-08-13 15:25:39 -0700

    Add err to error message

--- ygen/directory.go
+++ ygen/directory.go
@@ -164,7 +164,7 @@ func findSchemaPath(parent *Directory, fieldName string, shadowSchemaPaths, abso
 	for _, e := range util.SchemaEntryPathNoChoiceCase(field) {
 		im, err := e.InstantiatingModule()
 		if err != nil {
-			return nil, nil, fmt.Errorf("FindSchemaPath(shadowSchemaPaths:%v): cannot find instantiating module for field %q in Directory %s", shadowSchemaPaths, fieldName, parent.Path)
+			return nil, nil, fmt.Errorf("FindSchemaPath(shadowSchemaPaths:%v): cannot find instantiating module for field %q in Directory %s: err", shadowSchemaPaths, fieldName, parent.Path, err)
 		}
 		fieldSliceModules = append(fieldSliceModules, im)
 	}
@@ -219,7 +219,7 @@ func findMapPaths(parent *Directory, fieldName string, compressPaths, shadowSche
 	for _, e := range util.SchemaEntryPathNoChoiceCase(field) {
 		im, err := e.InstantiatingModule()
 		if err != nil {
-			return nil, nil, fmt.Errorf("FindSchemaPath(shadowSchemaPaths:%v): cannot find instantiating module for field %q in Directory %s", shadowSchemaPaths, fieldName, parent.Path)
+			return nil, nil, fmt.Errorf("FindSchemaPath(shadowSchemaPaths:%v): cannot find instantiating module for field %q in Directory %s: %v", shadowSchemaPaths, fieldName, parent.Path, err)
 		}
 		fieldSliceModules = append(fieldSliceModules, im)
 	}
