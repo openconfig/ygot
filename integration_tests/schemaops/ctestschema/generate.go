c1c0d8789f6145d34f3acfe96b302a882c18bbf5
wenovus <wenbli@google.com>
2023-04-20 14:23:56 -0700

    Refactor integration_test/schemaops to allow for fast iteration when developing generated code.

---
+++ integration_tests/schemaops/ctestschema/generate.go
@@ -0,0 +1,3 @@
+package ctestschema
+
+//go:generate ./update.sh
---
+++ integration_tests/schemaops/ctestschema/update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Hack to ensure that if we are running on OS X with a homebrew installed
+# GNU sed then we can still run sed.
+runsed() {
+  if hash gsed 2>/dev/null; then
+    gsed "$@"
+  else
+    sed "$@"
+  fi
+}
+
+go run ../../../generator/generator.go -path="." -output_file=ctestschema.go \
+  -package_name=ctestschema -generate_fakeroot -fakeroot_name=device \
+  -compress_paths \
+  -shorten_enum_leaf_names \
+  -typedef_enum_with_defmod \
+  -enum_suffix_for_simple_union_enums \
+  -generate_rename \
+  -generate_append \
+  -generate_getters \
+  -generate_leaf_getters \
+  -generate_simple_unions \
+  -annotations \
+  ../yang/ctestschema.yang
+gofmt -w -s ctestschema.go
